4439fdff11e445101dca54bbb30760807b40db3b
yanismnsr <44984759+yanismnsr@users.noreply.github.com>
2019-04-21 10:40:33 +0100

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,22 @@
+
+public class Player {
+	
+	private String name;
+	private int color;
+	
+	public Player(String name, int color) {
+		this.name = name;
+		this.color = color;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public int getColor() {
+		return this.color;
+	}
+	
+	
+
+}
