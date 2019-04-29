592e1148e61f37578581e8c3a24d949f4e3bf4ab
yanismnsr <44984759+yanismnsr@users.noreply.github.com>
2019-04-29 21:15:01 +0100

    Update Knight.java

--- Knight.java
+++ Knight.java
@@ -10,14 +10,32 @@ public class Knight extends Piece{
 	}
 
 
-	public ArrayList<Integer> movements(int position, Chessboard board){
+	public ArrayList<Integer> casesMangeables(Chessboard board){
 
 		ArrayList<Integer> liste = new ArrayList<Integer>();
 		int pos;
+		int index = board.indexOf(this);
+
+		for (int i = 0; i < listeMouvements.length; i++){
+			pos = Piece.getCase120(Piece.getCase64(index) + listeMouvements[i]);
+			if (pos != -1){
+				liste.add(pos);
+			}
+		}
+
+		return liste;
+
+	}
+
+
+	public ArrayList<Integer> movements(Chessboard board){
+
+		ArrayList<Integer> liste = new ArrayList<Integer>();
+		int pos;
+		int index = board.indexOf(this);
 
 		for (int i = 0; i < Knight.listeMouvements.length; i++){
-			System.out.println(Knight.listeMouvements[i]);
-			pos = Piece.getCase120(Piece.getCase64(position) + Knight.listeMouvements[i]);
+			pos = Piece.getCase120(Piece.getCase64(index) + Knight.listeMouvements[i]);
 			if (pos != -1 && board.getCase(pos).getColor() != this.getColor()){
 				liste.add(pos);
 			}
@@ -31,8 +49,9 @@ public class Knight extends Piece{
 		Piece p = b.getCase(1);
 		if (p instanceof Knight){
 			Knight k = (Knight)p;
-			ArrayList<Integer> a = k.movements(1, b);
+			ArrayList<Integer> a = k.movements(b);
 			System.out.println(a);
+			System.out.println(k.casesMangeables(b));
 		}
 	}
 
