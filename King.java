bf48984ed4acad65cb79aa26c27ce4ab5831ce84
yanismnsr <44984759+yanismnsr@users.noreply.github.com>
2019-04-29 22:21:29 +0100

    Update King.java

--- King.java
+++ King.java
@@ -1,8 +1,36 @@
+// class à modifier plutard (mouvements interdits)
+import java.util.ArrayList;
+
 
 public class King extends Piece{
-		
+
+	public static int[] listeMouvements = {-11, -10, -9, -1, 1, 9, 10, 11};
+
 	public King (int color) {
 		super(color);
 	}
-	
+
+	public ArrayList<Integer> movements(Chessboard board){
+
+		int pos;
+		int index = board.indexOf(this);
+		ArrayList<Integer> liste = new ArrayList<Integer>();
+
+		for (int i = 0; i < King.listeMouvements.length; i++){
+			pos = Piece.getCase120(Piece.getCase64(index) + King.listeMouvements[i]);
+			if (pos != -1 && board.getCase(pos).isEmpty()){
+				liste.add(pos);
+			}
+		}
+
+		return liste;
+
+	}
+
+	public static void main (String[] args){
+		Chessboard b = new Chessboard();
+		King k = (King)b.getCase(59);
+		System.out.println(k.movements(b));
+	}
+
 }
